66185922d5643b075aa9b8eb09b88080defd765f
Akhilkhandelwal15 <khandelwalakhil2003@gmail.com>
2026-01-01 16:28:07 +0530

    updated queryClient for refetchOnWindowFocus

--- src/queryClient.js
+++ src/queryClient.js
@@ -1,5 +1,12 @@
 import { QueryClient } from "react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false, // if it is true or not there then if i move out of the window then query will run again
+      retry: false,
+    },
+  },
+});
 
 export default queryClient;
--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -3,9 +3,7 @@ import { Navigate } from "react-router-dom";
 import { verifyUser } from "../api/authApi";
 
 const PrivateRoute = ({children})=>{
-  const {data: user, isLoading, isError} = useQuery(['me'], verifyUser, {
-    retry: false,
-  });
+  const {data: user, isLoading, isError} = useQuery(['me'], verifyUser);
   
   if (isLoading) return <div>Loading...</div>;
   if (isError || !user) return <Navigate to="/login" replace />;
